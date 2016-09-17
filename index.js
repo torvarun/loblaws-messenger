5a238a0fcf088ddaa74ccd672ab487ddcb78fdfd
Jordan <jordansne@gmail.com>
2016-09-17 04:35:44 -0400

    Cleanup code

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-'use strict'
+// Used for Atom editor
+/* jshint esversion: 6, node: true */
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+'use strict';
 
-app.set('port', (process.env.PORT || 5000))
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.set('port', (process.env.PORT || 5000));
 
-// Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// Index route
+// Set default index reponse
 app.get('/', function (req, res) {
-    res.send('loblaws-webhook')
-})
+    res.send('loblaws-webhook');
+});
 
-// for Facebook verification
+// Facebook webhook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'loblaws-webhook') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
-})
+    res.send('Error, wrong token');
+});
 
-// Spin up the server
+// Start the express server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+    console.log('Running on port', app.get('port'));
+});
 
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
+    for (var i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
@@ -59,4 +59,4 @@ function sendMessage(recipientId, message) {
             console.log('Error: ', response.body.error);
         }
     });
-};
+}
