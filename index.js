a9cd5a01c013daa0747d915674c79ffd12e8bdbe
torvarun <torvarun@gmail.com>
2016-09-17 02:25:28 -0400

    facebook msg

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1357261969')
 })
 
 // for Facebook verification
